cd3fde8aeab3913927003bf6c23b7c3fafc5a858
Kishori Kumar <kishori@synopsys.com>
2022-07-19 01:49:55 +0530

    data input test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import {DATA_1} from './inputs'
+import {DATA_2} from './inputs'
+
 async function run() {
     console.log("Main is running")
+    console.log("Passed Data 1 is : " + DATA_1);
+    console.log("Passed Data 2 is : " + DATA_2)
 }
 
 run()
